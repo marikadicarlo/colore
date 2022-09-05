4ab38247beb22b59bcdf0f224554ca33fbdf3b9e
marikadicarlo <mdicarlo19@yahoo.com>
2022-09-05 11:47:25 -0400

    generated and RGB color value

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+// Generate random numbers and console log to check
 function randomInteger(max) {
     return Math.floor(Math.random()*(max + 1));
 }
 
-// Outputs a number between 0 and 225 inclusive
-console.log(randomInteger(225));
+// Outputs a number between 0 and 255 inclusive
+// console.log(randomInteger(255));
+
+// Create three random integers between 0 and 255
+// There values will represent the red, green, and blue components of our colors
+
+function randomRgbColor() {
+    let r = randomInteger(255);
+    let g = randomInteger(255);
+    let b = randomInteger(255);
+    return [r,g,b];
+}
+
+for (i = 0; i < 5; i++) {
+    console.log(randomRgbColor());
+}
